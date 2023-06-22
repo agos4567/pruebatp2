e50cf417c0138754e200c33fa03efb3df6afe551
agos4567 <agosgalfre23@gmail.com>
2023-06-21 22:18:02 -0300

    agrego

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, Agos!");
+
+// int a;
+// int b;
+
+// a=10;
+// b=a;
+
+// Console.WriteLine("Valor de a : {0}",+a);
+
+// Console.WriteLine("Valor de b : {0}",+b);
+
+float floatNum1=0;
+
+bool anda =false;
+string? numString="";
+
+while (!anda)
+{
+    Console.WriteLine("Ingrese un numero valido: ");
+    numString=Console.ReadLine();
+    anda =float.TryParse(numString, out floatNum1);
+   
+    if (!anda)
+    {
+        Console.WriteLine(numString + "no es un numero valido:  ");
+    }else{
+        Console.WriteLine(numString +  "si es un numero valido:  ");
+    }
+
+}
+
+
+//para invertir el numero
+
+
+
+int ReverseNumber(int number)
+{
+    int reversedNumber=0;
+  while (number !=0)
+  {
+    int digit= number %10;
+    reversedNumber=reversedNumber * 10 + digit;
+    number /=10;
+  }
+
+
+  return reversedNumber;
+}
+int intNum= (int)floatNum1;
+
+int reversedNum=ReverseNumber(intNum);
+// Console.WriteLine( reversedNum + "Numero invertido: ");
+Console.WriteLine(  "Numero invertido: "  + reversedNum );
+
+// // int total;
+// // total =ReverseNumber;
+
+// Console.WriteLine(ReverseNumber());
